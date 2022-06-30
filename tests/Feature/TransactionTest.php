4951c4a85a3dfaa866b6f9cb968428336a839e13
Giovanne Oliveira <giovanne@giovanne.dev>
2022-06-30 20:38:31 +0000

    chore(tests): add more transaction tests

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -34,12 +34,6 @@ class TransactionTest extends TestCase
         $this->setAccountBalance($person_account, 100);
         $this->setAccountBalance($store_account, 0);
 
-        /*$response = $this->createTransaction([
-            'payerId' => $person_account->id,
-            'payeeId' => $store_account->id,
-            'amount' => 50,
-        ]);*/
-
         $response = $this->post('/api/transaction/create', [
             'payer' => $person->id,
             'payee' => $store->id,
@@ -49,5 +43,77 @@ class TransactionTest extends TestCase
         $this->assertTrue($response->status() == 201);
         $this->assertEquals(50, $person->fresh()->account()->first()->balance);
         $this->assertEquals(50, $store->fresh()->account()->first()->balance);
+    }
+
+    public function test_store_cant_transfer_money_to_user()
+    {
+        $person = $this->createUser([
+            'document' => 12345678900,
+            'is_store' => false,
+        ]);
+
+        $store = $this->createUser([
+            'document' => 98765432100,
+            'is_store' => true,
+        ]);
+
+        $person_account = $this->createAccount([
+            'userId' => $person->id,
+            'isActive' => true
+        ]);
+
+        $store_account = $this->createAccount([
+            'userId' => $store->id,
+            'isActive' => true
+        ]);
+
+        $this->setAccountBalance($person_account, 0);
+        $this->setAccountBalance($store_account, 100);
+
+        $response = $this->post('/api/transaction/create', [
+            'payer' => $store->id,
+            'payee' => $person->id,
+            'amount' => 50,
+        ]);
+
+        $this->assertTrue($response->status() == 401);
+        $this->assertEquals(0, $person->fresh()->account()->first()->balance);
+        $this->assertEquals(100, $store->fresh()->account()->first()->balance);
+    }
+
+    public function test_user_cant_transfer_money_without_balance()
+    {
+        $person = $this->createUser([
+            'document' => 12345678900,
+            'is_store' => false,
+        ]);
+
+        $store = $this->createUser([
+            'document' => 98765432100,
+            'is_store' => true,
+        ]);
+
+        $person_account = $this->createAccount([
+            'userId' => $person->id,
+            'isActive' => true
+        ]);
+
+        $store_account = $this->createAccount([
+            'userId' => $store->id,
+            'isActive' => true
+        ]);
+
+        $this->setAccountBalance($person_account, 0);
+        $this->setAccountBalance($store_account, 100);
+
+        $response = $this->post('/api/transaction/create', [
+            'payer' => $person->id,
+            'payee' => $store->id,
+            'amount' => 50,
+        ]);
+
+        $this->assertTrue($response->status() == 422);
+        $this->assertEquals(0, $person->fresh()->account()->first()->balance);
+        $this->assertEquals(100, $store->fresh()->account()->first()->balance);
     }
 }
